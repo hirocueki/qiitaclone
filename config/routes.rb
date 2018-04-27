8c816a995dc91f707383107133c863b7840f013c
hirocueki <hirocueki@gmail.com>
2018-04-27 18:42:25 +0900

    add ユーザーページにアイテム一覧追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :items
   root to: 'users#index'
   get    '/signup',  to: 'users#new'
   get     '/login',  to: 'sessions#new'
   post    '/login',  to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    resources :items
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427045051) do
+ActiveRecord::Schema.define(version: 20180427093729) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
